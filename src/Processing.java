c7b33411dc90618f01582b7a15752b3034d2936f
Tomas Frohawk <tom.frohawk@hotmail.co.uk>
2017-12-04 22:28:30 +0000

    Minor changes

--- src/Processing.java
+++ src/Processing.java
@@ -1,22 +1,34 @@
 import java.io.*;
+import java.util.Scanner;
+import java.util.Date;
 
 public class Processing {
 	void logSkill(String skill) {
 		String fileName = "xprecords.txt";
-		String line = null;
-
+		//String line = null;
+		Date now = new Date();
+		
+		Scanner input = new Scanner(System.in);
+		
 		switch (skill) {
 		case "Agility":
 			System.out.println("Agility");
 
 			try {
-				FileWriter fileWriter = new FileWriter(fileName);
+				FileWriter fileWriter = new FileWriter(fileName, true);
 
 				// Always wrap FileReader in BufferedReader.
 				BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
 
-				bufferedWriter.write("Test");
-
+				// write details to file
+				bufferedWriter.write(skill);
+				bufferedWriter.newLine();
+				System.out.println("What is current xp?");
+				bufferedWriter.write(input.next());
+				bufferedWriter.newLine();
+				bufferedWriter.write(now.toString());
+				bufferedWriter.newLine();
+				
 				// Always close files.
 				bufferedWriter.close();
 
@@ -31,5 +43,7 @@ public class Processing {
 		default:
 			System.out.println("Unknown skill!");
 		}
+		
+		input.close();
 	}
 }
