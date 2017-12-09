5c2da852c91806afc013fef4efd66877b9a8761a
Tomas Frohawk <tom.frohawk@hotmail.co.uk>
2017-12-09 17:41:13 +0000

    Added new command for getting player's stat by skill

--- src/Commands.java
+++ src/Commands.java
@@ -7,27 +7,35 @@ public class Commands {
 		Scanner input = new Scanner(System.in);
 
 		System.out
-				.println("What would you like to do? (1=Log XP Update, 2=Get Player Stats, 3=Update CML, q=quit)");
+				.println("What would you like to do? (1=Log XP Update, 2=Get Player stat By Skill, 3=Get Player Stats, 4=Update CML, q=quit)");
 		command = input.next();
 
 		Processing process = new Processing();
 
 		switch (command) {
 		case "1":
+			// command to log an xp update to the txt file
 			System.out.println("Which skill?");
 
 			process.logSkill(input.next());
 			break;
 		case "2":
+			// command to get players stat by skill, i.e. agility
+			System.out.println("Which player?");
+
+			
+			break;
+		case "3":
+			// command to get all player stats
 			System.out.println("Which player?");
 
 			process.processURL(input.next());
 			break;
-		case "3":
-
+		case "4":
+			// command to update top level 3's in CML
 			try {
 				BufferedReader br = new BufferedReader(new FileReader(
-						"zplayers.txt"));
+						"toplvl3.txt"));
 
 				for (String line = br.readLine(); line != null; line = br
 						.readLine()) {
@@ -42,6 +50,7 @@ public class Commands {
 			
 			break;
 		case "q":
+			// command to quit the application
 			System.out.println("Program is terminating!");
 			break;
 
